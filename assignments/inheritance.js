da79a21b0be7e9949f7af7e28d7912b5aaf7c669
Saranya <281651861+saranyakundhi0901@users.noreply.github.com>
2026-08-29 19:32:37 +0530

    Refactor inheritance.js to streamline inheritance examples and improve code clarity

--- assignments/inheritance.js
+++ assignments/inheritance.js
@@ -1,206 +1,117 @@
-
-//TYPES OF INHERITANCE IN JAVASCRIPT
-
 // 1. SINGLE INHERITANCE
-
-
-// Parent class
-class Animal {
-    eat() {
-        console.log("Animal can eat");
-    }
-}
-
-
-class Dog extends Animal {
-    bark() {
-        console.log("Dog can bark");
-    }
-}
-
-console.log("----- 1. Single Inheritance -----");
-
-let dog = new Dog();
-
-dog.eat();      
-dog.bark();     
-
-
-
-
-// 2. MULTILEVEL INHERITANCE
-
-
-// Grandparent class
-class LivingThing {
-    breathe() {
-        console.log("Living thing can breathe");
-    }
-}
-
-// Animal2 inherits LivingThing
-class Animal2 extends LivingThing {
-    walk() {
-        console.log("Animal can walk");
-    }
-}
-
-
-class Dog2 extends Animal2 {
-    bark() {
-        console.log("Dog can bark");
-    }
-}
-
-console.log("\n----- 2. Multilevel Inheritance -----");
-
-let dog2 = new Dog2();
-
-dog2.breathe();     
-dog2.walk();        
-dog2.bark();       
-
-
-
-
-// 3. HIERARCHICAL INHERITANCE
-
-
-// Parent class
+console.log("Single Inheritance");
 class Vehicle {
     start() {
-        console.log("Vehicle starts");
+        console.log("Vehicle is starting.");
     }
 }
-
-// Car inherits Vehicle
 class Car extends Vehicle {
     drive() {
-        console.log("Car is driving");
+        console.log("Car is driving.");
     }
 }
-
-
-class Bike extends Vehicle {
-    ride() {
-        console.log("Bike is riding");
-    }
-}
-
-console.log("\n----- 3. Hierarchical Inheritance -----");
-
 let car = new Car();
+car.start();
+car.drive();
 
-car.start();        
-car.drive();        
+// 2. MULTILEVEL INHERITANCE
+console.log("\nMultilevel Inheritance");
+class Vehicle2 {
+    start() {
+        console.log("Vehicle is starting.");
+    }
+}
+class Car2 extends Vehicle2 {
+    drive() {
+        console.log("Car is driving.");
+    }
+}
+class SportsCar extends Car2 {
+    boost() {
+        console.log("SportsCar is boosting speed.");
+    }
+}
+let sportsCar = new SportsCar();
+sportsCar.start();
+sportsCar.drive();
+sportsCar.boost();
 
+// 3. HIERARCHICAL INHERITANCE
+console.log("\nHierarchical Inheritance");
+class Vehicle3 {
+    start() {
+        console.log("Vehicle is starting.");
+    }
+}
+class Car3 extends Vehicle3 {
+    drive() {
+        console.log("Car is driving.");
+    }
+}
+class Bike extends Vehicle3 {
+    pedal() {
+        console.log("Bike is pedaling.");
+    }
+}
+let car3 = new Car3();
 let bike = new Bike();
-
-bike.start();       
-bike.ride();        
-
-
-
+car3.start();
+car3.drive();
+bike.start();
+bike.pedal();
 
 // 4. MULTIPLE INHERITANCE
-
-
-// First parent class
-class Father {
-    fatherProperty() {
-        console.log("Property from Father");
+// Using Mixins
+console.log("\nMultiple Inheritance");
+const GPSEnabled = {
+    locate() {
+        console.log("Locating position via GPS.");
+    }
+};
+const Bluetooth = {
+    connect() {
+        console.log("Connecting via Bluetooth.");
+    }
+};
+class SmartCar {
+    honk() {
+        console.log("SmartCar is honking.");
     }
 }
-
-// Second parent class
-class Mother {
-    motherProperty() {
-        console.log("Property from Mother");
-    }
-}
-
-// Child class
-class Child {
-    childProperty() {
-        console.log("Property from Child");
-    }
-}
-
-// Mixin:
-// Copy methods from Father into Child
-Object.getOwnPropertyNames(Father.prototype).forEach((method) => {
-
-    if (method !== "constructor") {
-        Child.prototype[method] = Father.prototype[method];
-    }
-
-});
-
-// Copy methods from Mother into Child
-Object.getOwnPropertyNames(Mother.prototype).forEach((method) => {
-
-    if (method !== "constructor") {
-        Child.prototype[method] = Mother.prototype[method];
-    }
-
-});
-
-console.log("\n----- 4. Multiple Inheritance -----");
-
-let child = new Child();
-
-child.fatherProperty();     
-child.motherProperty();     
-child.childProperty();      
-
-
+// Add both GPSEnabled and Bluetooth features
+Object.assign(SmartCar.prototype, GPSEnabled, Bluetooth);
+let smartCar = new SmartCar();
+smartCar.honk();
+smartCar.locate();
+smartCar.connect();
 
 // 5. HYBRID INHERITANCE
-
-
-// Parent class
-class Person {
-    introduce() {
-        console.log("I am a person");
+console.log("\nHybrid Inheritance");
+class Vehicle4 {
+    start() {
+        console.log("Vehicle is starting.");
     }
 }
-
-// Student inherits Person
-// Person -> Student
-class Student extends Person {
-    study() {
-        console.log("Student is studying");
+class Car4 extends Vehicle4 {
+    drive() {
+        console.log("Car is driving.");
     }
 }
-
-// Another independent class
-class Sports {
-    play() {
-        console.log("Student plays sports");
+class Bike4 extends Vehicle4 {
+    pedal() {
+        console.log("Bike is pedaling.");
     }
 }
-
-
-class CollegeStudent extends Student {
-    attendCollege() {
-        console.log("Student attends college");
+const Turbo = {
+    boost() {
+        console.log("Turbo boost activated.");
     }
-}
-
-// Add Sports methods to CollegeStudent using Mixin
-Object.getOwnPropertyNames(Sports.prototype).forEach((method) => {
-
-    if (method !== "constructor") {
-        CollegeStudent.prototype[method] = Sports.prototype[method];
-    }
-
-});
-
-console.log("\n----- 5. Hybrid Inheritance -----");
-
-let student = new CollegeStudent();
-
-student.introduce();        
-student.study();            
-student.attendCollege();   
-student.play();             
+};
+Object.assign(Car4.prototype, Turbo);
+let car4 = new Car4();
+let bike4 = new Bike4();
+car4.start();
+car4.drive();
+car4.boost();
+bike4.start();
+bike4.pedal();
