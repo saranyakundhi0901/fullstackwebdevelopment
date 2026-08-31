5b246502c878020fdb2329c5708e178b781fe8c0
Kundi Saranya <saranyakundhi@gmail.com>
2026-08-31 19:28:32 +0530

    Refactor inheritance and add Student, Bank classes
    Refactor Animal class and implement Student and Bank classes with encapsulation.

--- records/week-4/inheritance.js
+++ records/week-4/inheritance.js
@@ -1,20 +1,72 @@
-//parent class
+class Student {
+
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    display() {
+        console.log("Name:", this.name);
+        console.log("Age:", this.age);
+    }
+}
+
+let s1 = new Student("Snoopy", 2);
+
+s1.display();
+
 class Animal{
-    constructor(name){
-        this.name=name;
+
+    eat()
+    {
+    console.log("Eating");
     }
-    display(){
-        console.log("Animal name:"+this.name);
-    }
+
 }
-//child class
+
 class Dog extends Animal{
-    sound(){
-        console.log(this.name + " says Bark!");
+
+    bark()
+    {
+    console.log("Barking");
+    }
+
+}
+
+const d=new Dog();
+
+d.eat();
+d.bark();
+
+
+class Bank {
+
+    #balance = 10000;
+
+    // Getter method
+    get balance() {
+        return this.#balance;
+    }
+
+    // Setter method
+    set balance(amount) {
+        if (amount >= 0) {
+            this.#balance = amount;
+        } else {
+            console.log("Balance cannot be negative.");
+        }
     }
 }
-//creating object
-let dog=new Dog("Tommy");
-//calling method
-dog.display();
-dog.sound();
+
+let account = new Bank();
+
+// Using getter
+console.log("Initial Balance:", account.balance);
+
+// Using setter
+account.balance = 15000;
+
+console.log("Updated Balance:", account.balance);
+
+// Invalid update
+account.balance = -500;
